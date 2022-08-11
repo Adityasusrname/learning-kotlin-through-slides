c7b15c39bde00afde197fa0def8e4d93df024b66
Aditya Srivastava <adityasmailbooxx@gmail.com>
2022-08-11 10:43:34 +0530

    Added ExperimentsWithNull.kt

---
+++ src/main/kotlin/ExperimentsWithNull.kt
@@ -0,0 +1,32 @@
+class ExperimentsWithNull {
+    var numberOfBooks = 6
+
+    fun trying_question_mark_operator(){
+       numberOfBooks= numberOfBooks?.dec()
+        println(numberOfBooks)
+    }
+    fun trying_out_double_exclamation_operator(){
+        var a = 5
+        try {
+            val len = a!!
+        }
+        catch(error:Error){
+
+            println(error)
+
+        }
+
+
+    }
+    fun trying_out_elvis_operator(){
+       val check = null?:100
+        println(check)
+    }
+}
+
+fun main(){
+    val ExperimentsWithNull = ExperimentsWithNull()
+    ExperimentsWithNull.trying_question_mark_operator()
+    ExperimentsWithNull.trying_out_double_exclamation_operator()
+    ExperimentsWithNull.trying_out_elvis_operator()
+}
