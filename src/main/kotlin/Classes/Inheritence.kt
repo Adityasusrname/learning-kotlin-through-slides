cc0a5fa11bd0958c403eca7463446a8c9719327e
Aditya Srivastava <adityasmailbooxx@gmail.com>
2022-08-11 16:44:50 +0530

    Added Inheritence.kt

--- src/main/kotlin/Classes/Inheritence.kt
+++ src/main/kotlin/Classes/Inheritence.kt
@@ -1,4 +1,18 @@
 package Classes
 
-class Inheritence {
+open class Superclass{
+    open fun speak(){
+        println("Hello!")
+    }
+}
+
+class Subclass:Superclass(){
+    override fun speak() {
+        println("Bye!")
+    }
+}
+
+fun main(){
+    val Sublcass =Subclass()
+    Sublcass.speak()
 }
