06b29b4a51eee1c2be7c18a213be93c78ab1b3cf
Aditya Srivastava <adityasmailbooxx@gmail.com>
2022-08-11 09:45:45 +0530

    Added When.kt

---
+++ src/main/kotlin/When.kt
@@ -0,0 +1,17 @@
+class When {
+    val results=50
+
+    fun check_results(){
+        when(results){
+            0->println("No results")
+            in 1..39 -> println("Got results!")
+            else -> println("That's a lot of results!")
+        }
+    }
+
+}
+
+fun main(){
+    val whenObject = When()
+    whenObject.check_results()
+}
