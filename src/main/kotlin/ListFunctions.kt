9ecb4cdd75e9fbef78ea1132020f2f145426523c
Aditya Srivastava <adityasmailbooxx@gmail.com>
2022-08-11 13:01:24 +0530

    Added ListFunctions.kt

---
+++ src/main/kotlin/ListFunctions.kt
@@ -0,0 +1,34 @@
+class ListFunctions {
+    val books = listOf("nature","biology","birds")
+    val numbers = listOf(1,2,3,4,5)
+
+    fun print_filtered_books(){
+        println(books.filter{
+            it[0] == 'b'
+        })
+    }
+
+    fun sequence_to_list(){
+        val filtered = books.asSequence().filter {
+            it[0] == 'b'
+        }
+        println(filtered.toList())
+    }
+
+    fun using_map(){
+        val newNumbers=numbers.map {
+            it*2
+        }
+        println(newNumbers)
+    }
+
+}
+
+fun main(){
+    val ListFunctions = ListFunctions()
+    ListFunctions.print_filtered_books()
+    ListFunctions.sequence_to_list()
+    ListFunctions.using_map()
+    val newList = listOf(ListFunctions.books,ListFunctions.numbers)
+    println(newList.flatten())
+}
