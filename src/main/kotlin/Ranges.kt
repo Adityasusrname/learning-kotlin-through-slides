3f7e0bd866cce22985def844dd4fe6d56a3d0937
Aditya Srivastava <adityasmailbooxx@gmail.com>
2022-08-11 09:40:59 +0530

    Added Ranges.kt

---
+++ src/main/kotlin/Ranges.kt
@@ -0,0 +1,13 @@
+class Ranges {
+    val numberOfStudents = 50
+    fun check_for_range(){
+        if(numberOfStudents in 1..100){
+            println(numberOfStudents)
+        }
+    }
+}
+
+fun main(){
+    val ranges=Ranges()
+    ranges.check_for_range()
+}
